6cffd73a5d590bf53e5ccd45a24c1898d4b04e04
boby kumar <bobykumar.info7@gmail.com>
2021-05-25 18:45:48 +0530

    after reset

--- app/src/main/java/com/example/myapp/MainActivity.java
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -17,6 +17,8 @@ public class MainActivity extends AppCompatActivity {
         titleText.setText("GitHub");
         TextView subjectText=findViewById(R.id.subject);
         subjectText.setText("version controlled  and Git ");
+        TextView subjectInfoText=findViewById(R.id.subject_info);
+        subjectInfoText.setText("I just said \"version control\". Now since you're in this course, you might already know a bit about version control. But if you don't, a handy trick I've learned is to try reading the words in reverse - so \"version control\" would become \"control version\"! So a Version Control System is just software that helps you control (or manage) the different versions...of something (typically source code)");
 
 
     }
